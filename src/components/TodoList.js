261564850b64de1ba73722fbb4bacf42f53bd9f0
Manson <manson972@yahoo.com>
2022-11-01 14:12:28 +0800

    create task update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,14 +7,25 @@ const TodoList = () => {
         setModal(!modal);
     }
 
+    const [taskList, setTaskList] = useState([]);
+
+    const saveTask = (taskObj) => {
+      let tempList = taskList;
+      tempList.push(taskObj);
+      setTaskList(tempList);
+      setModal(!modal);
+    }
+
     return (
     <>
       <div className="header text-center">
-        <h3>Todo List</h3>
-        <button className="btn btn-primary mt-2" onClick={() => { setModal(true); }}>Create Task</button>
+          <h3>Todo List</h3>
+          <button className="btn btn-primary mt-2" onClick={() => { setModal(true); }}>Create Task</button>
       </div>
-      <div className="task-container"></div>
-      <CreateTask modal={modal} toggle={toggle} />
+      <div className="task-container">
+          {taskList.map((obj) => <li>{obj.taskTitle}</li>)}
+      </div>
+      <CreateTask modal={modal} toggle={toggle} saveTask={saveTask} />
     </>
   );
 }
--- src/modals/CreateTask.js
+++ src/modals/CreateTask.js
@@ -1,27 +1,48 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const createTask = ({ modal, toggle }) => {
+const CreateTask = ({ modal, toggle, saveTask }) => {
     const [taskTitle, setTaskTitle] = useState('');
     const [description, setDescription] = useState('');
-    
+
+    const handleChange = (e) => {
+        //const {name, value} = e.target
+        const name = e.target.name;
+        const value = e.target.value;
+
+        if (name === "taskTitle") {
+            setTaskTitle(value);
+        }else if(name === "description") {
+            setDescription(value);
+        }
+    }
+
+    const handleSave = () => {
+        let taskObj = {};
+        taskObj.taskTitle = taskTitle;
+        taskObj.description = description;
+        saveTask(taskObj);
+    }
+
     return (
         <Modal isOpen={modal} toggle={toggle}>
             <ModalHeader toggle={toggle}>Create Task</ModalHeader>
             <ModalBody>
                 <form>
-                    <div className='form-group'>
+                    <div className="form-group">
                         <label>Task Title</label>
-                        <input type="text" className='form-control'></input>
+                        <input type="text" className="form-control" name="taskTitle" 
+                            value={taskTitle} onChange={handleChange}></input>
                     </div>
-                    <div className='form-group mt-3'>
+                    <div className="form-group mt-3">
                         <label>Description</label>
-                        <textarea rows="5" className='form-control'></textarea>
+                        <textarea rows="5" className="form-control" name="description" 
+                            value={description} onChange={handleChange}></textarea>
                     </div>
                 </form>
             </ModalBody>
             <ModalFooter>
-                <Button color="primary" onClick={toggle}>
+                <Button color="primary" onClick={handleSave}>
                     Create
                 </Button>{' '}
                 <Button color="secondary" onClick={toggle}>
@@ -32,4 +53,4 @@ const createTask = ({ modal, toggle }) => {
     );
 };
 
-export default createTask;
+export default CreateTask;
